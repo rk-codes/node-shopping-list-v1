1c3c7cfbcf6af721f13903f80280b95d3ed3f343
rk-codes <ping.you.back@gmail.com>
2017-11-07 12:36:22 -0800

    add recipes endpoint

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -18,10 +18,20 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+// add some recipes
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar'])
+Recipes.create('cookie', ['flour', 'butter', 'sugar']);
+Recipes.create('milkshake', ['milk', 'icecream', 'cocoa']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+//return all recipes
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
